f4893d9721b3bb923cce7eee3e96f26c59a996ac
Joel Koen <mail@joelkoen.com>
2023-10-14 13:05:30 +1000

    build: add nix flake

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  inputs = {
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs = {
+        flake-utils.follows = "flake-utils";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, crane, fenix, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      packages.default =
+        let
+          craneLib = crane.lib.${system}.overrideToolchain
+            fenix.packages.${system}.minimal.toolchain;
+          pkgs = import nixpkgs { inherit system; };
+        in
+
+        with pkgs;
+
+        craneLib.buildPackage {
+          src = ./.;
+
+          nativeBuildInputs = [ pkg-config ];
+
+          buildInputs = [ openssl ];
+        };
+    });
+}
